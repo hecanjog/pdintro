3afbeb49c834a16098d06df1d910fd2cd1c40822
Erik Schoster <erik@hecanjog.com>
2014-05-30 19:58:50 -0500

    update dynamics graph

--- source/dynamics.py
+++ source/dynamics.py
@@ -7,6 +7,8 @@ d3 = [ dsp.randchoose([1,4,5,8]) for p in x ]
 
 d7 = [ dsp.randint(1, 8) for p in x ]
 
+pp.subplot(211)
+
 pp.plot(x, d3, 
     marker='x', 
     linestyle=' ', 
@@ -14,10 +16,21 @@ pp.plot(x, d3,
     label='2bit Dynamics'
 )
 
+pp.xlabel('Time')
+pp.ylabel('Loudness')
+#pp.grid(True)
+pp.ylim(0, 9)
+pp.yticks(range(9), ['', 'ppp', 'pp', 'p', 'mp', 'mf', 'f', 'ff', 'fff', 'ffff', ''])
+pp.xlim(-1,32)
+pp.xticks(range(34), [ '' for t in range(34) ])
+pp.legend()
+
+pp.subplot(212)
+
 pp.plot(x, d7, 
     marker='o', 
     linestyle=' ', 
-    color='y', 
+    color='b', 
     label='3bit Dynamics'
 )
 
